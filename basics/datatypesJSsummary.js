a59e1c89e605b1c7c690a2246239cef1b9a2dc8a
PoonamMehan <116332299+PoonamMehan@users.noreply.github.com>
2024-07-01 10:34:36 +0000

    add datatype summary

---
+++ basics/datatypesJSsummary.js
@@ -0,0 +1,48 @@
+// let userEmail;
+// console.log(typeof(userEmail));
+
+// const id = Symbol("123")
+// const anotherId = Symbol("123")
+// console.log(id==anotherId)
+
+
+// const myFunc = function(num, displayFunc){
+//     displayFunc(num);
+//     console.log("Does upon defining a function with its name makes it run too?", num, "let's figure out how to pass arguments.")
+// }
+
+// const displayfunc = function(n){
+//     console.log(n);
+// }
+// myFunc(25, displayfunc);
+
+// const arr = [2, 3, 4]
+// const obj = {
+//     name: 'Poonam',
+//     age: 19,
+//     courseName: "B.tech CSE"
+// }
+
+// const myFunc = function(){
+//     console.log("Hello World!")
+// }
+
+// console.log(typeof arr);
+// console.log(typeof obj);
+// console.log(typeof myFunc);
+
+
+const str = "I'm a string"
+const num = 2
+const imNull = null
+const imUndef = undefined
+const imBool = true
+const bigNumber = 33333333333333333334444444444n
+const imaSymbol = Symbol(554)
+console.log(typeof str)
+console.log(typeof num)
+console.log(typeof imNull)
+console.log(typeof imUndef)
+console.log(typeof imBool)
+console.log(typeof bigNumber)
+console.log(typeof imaSymbol)
