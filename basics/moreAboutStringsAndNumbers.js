92178018bb0bb738b69c88702140128bd910c249
PoonamMehan <116332299+PoonamMehan@users.noreply.github.com>
2024-07-02 20:21:20 +0000

    Add code files till the end of part1

---
+++ basics/moreAboutStringsAndNumbers.js
@@ -0,0 +1,19 @@
+// const num = "JITHE DEKHAN TU HI DISJE BHULEKHE BAN RAKAANE"
+// console.log(`kya ismein backslash n kaam karega?\nkiya kya? haa kiya or ab tab bhi dekh lete hain \t kiya hi hoga woh toh kiya, ab ariable inject karte hain idhar apan ${num} or pata karte hain ki var baad mein define karne se koi problem toh nahi aa jati kahi, aati hai yaar. Ek baari variable ke andar backslash n ya t dekhte hain AA RAHI HAI DIKKAT BACKSLASH T KO SAHI SE INTERPRET KARNE MEIN`)
+
+
+// Date and time
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toTimeString())
+console.log(myDate.toUTCString())
+console.log(myDate.getUTCMilliseconds())
+
+//end of part1
